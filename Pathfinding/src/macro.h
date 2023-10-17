a08520f8de840c8afa3e1e586f778979966f4ecb
Hugo Carricart <camaro33240@gmail.com>
2023-10-17 21:43:11 +0200

    First commit
    Base du code cpp avec CMakeList + lib SFML inclus.

---
+++ Pathfinding/src/macro.h
@@ -0,0 +1,13 @@
+﻿/**
+* @file macro.h
+* @brief 
+* 
+* @author hugo.carricart@ynov.com 
+* 
+* @copyright TeamRandom (c) 2023
+* @version 0.1
+* 
+* @date 12/10/2023
+*/
+
+#define DELETE_PTR(ptr) if(ptr != nullptr) { delete ptr; ptr = nullptr; }
---
+++ Pathfinding/src/main.cpp
@@ -0,0 +1,36 @@
+﻿/**
+* @file main.cpp
+* @brief
+*
+* @author hugo.carricart@ynov.com
+*
+* @copyright TeamRandom (c) 2023
+* @version 0.1
+*
+* @date 12/10/2023
+*/
+#include "macro.h"
+
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        for (auto event = sf::Event{}; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+}
