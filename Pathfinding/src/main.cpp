6d79cf4bc26a01618aa1ef618f5c0e2f636abdad
Hugo Carricart <camaro33240@gmail.com>
2023-10-19 18:32:03 +0200

    Ajout ImGui + Link avec SFML .
    Compil OK

--- Pathfinding/src/main.cpp
+++ Pathfinding/src/main.cpp
@@ -11,13 +11,14 @@
 */
 #include "macro.h"
 
+/*
 #include <iostream>
 
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "ThePathfinder2000" };
     window.setFramerateLimit(144);
 
     while (window.isOpen())
@@ -33,4 +34,50 @@ int main()
         window.clear();
         window.display();
     }
+}
+
+*/
+
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ThePathFinder2000");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(window, event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::ShowDemoWindow(); // Manque un truc.
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
 }
