9d97037ea0f506765ba576bf94f6526c9779dc44
Yoan Laurain <yoan.laurain0@gmail.com>
2023-10-21 19:35:07 +0200

    Génération de grid, placement debut, fin , murs
    La grille est générer en 3x3 et peut placer des meurs sur le clic molette, le point de départ ( unique ) sur le clic gauche et enfin l'arriver ( unique ) sur le clic droit

--- Pathfinding/src/StateMachine/GameState/GameState.h
+++ Pathfinding/src/StateMachine/GameState/GameState.h
@@ -19,11 +19,32 @@ public:
 	void Draw(float dt);
 
 private : 
+
+	void InitGridTiles();
+	void PlacePiece(sf::Mouse::Button);
+
+	void ResetStartPoint(int column, int row);
+	void ResetEndPoint(int column, int row);
+	void ResetWall(int column, int row);
+	
+
+	void PlaceStartPoint(int column, int row);
+	void PlaceEndPoint(int column, int row);
+	void PlaceWall(int column, int row);
 	
 	GameDataRef _data;
 
+	bool StartPlaced;
+	bool EndPlaced;
+
+	sf::Vector2i startingPoint;
+	sf::Vector2i endingPoint;
+
 	sf::Sprite _background;
 	sf::Sprite _pauseButton;
+	sf::Sprite _gridSprite;
+	sf::Sprite _gridPieces[3][3];
+	int _gridArray[3][3];
 };
 
 #endif /* GAME_STATE_H */
